a7867486c3cca4c8d4868898a56440a41afae7e2
Alex Gaubert <alex.gaubert@outlook.fr>
2016-03-22 17:53:14 +0100

    MAJ desc prog

--- bdd/casnalplus.sql
+++ bdd/casnalplus.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 22 Mars 2016 à 15:50
+-- Généré le :  Mar 22 Mars 2016 à 17:51
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -99,6 +99,7 @@ CREATE TRIGGER `before_insert_avis` BEFORE INSERT ON `avis`
 END
 //
 DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
@@ -209,6 +210,58 @@ CREATE TABLE IF NOT EXISTS `diffusion` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Contenu de la table `diffusion`
+--
+
+INSERT INTO `diffusion` (`CODE`, `ID`, `ID_1`) VALUES
+('1', '1', '2'),
+('10', '9', '4'),
+('11', '16', '1'),
+('12', '18', '1'),
+('13', '18', '2'),
+('14', '19', '2'),
+('15', '17', '4'),
+('16', '18', '4'),
+('17', '1', '1'),
+('18', '3', '1'),
+('19', '5', '1'),
+('2', '13', '3'),
+('20', '4', '2'),
+('21', '6', '2'),
+('22', '5', '3'),
+('23', '6', '3'),
+('24', '3', '4'),
+('25', '4', '4'),
+('26', '6', '4'),
+('27', '7', '1'),
+('28', '10', '1'),
+('29', '12', '1'),
+('3', '14', '1'),
+('30', '8', '2'),
+('31', '9', '2'),
+('32', '11', '2'),
+('33', '10', '3'),
+('34', '11', '3'),
+('35', '12', '3'),
+('36', '11', '4'),
+('37', '12', '4'),
+('38', '20', '2'),
+('39', '21', '2'),
+('4', '15', '4'),
+('40', '22', '2'),
+('41', '21', '3'),
+('42', '22', '3'),
+('43', '22', '4'),
+('44', '23', '1'),
+('45', '24', '2'),
+('46', '23', '4'),
+('5', '16', '2'),
+('6', '2', '3'),
+('7', '18', '3'),
+('8', '19', '3'),
+('9', '20', '1');
+
+--
 -- Déclencheurs `diffusion`
 --
 DROP TRIGGER IF EXISTS `before_insert_diffusion`;
@@ -222,58 +275,6 @@ CREATE TRIGGER `before_insert_diffusion` BEFORE INSERT ON `diffusion`
 END
 //
 DELIMITER ;
--- --------------------------------------------------------
---
--- Contenu de la table `diffusion`
---
-
-INSERT INTO `diffusion` (`CODE`, `ID`, `ID_1`) VALUES
-('1', '1', '2'),
-('10', '9', '4'),
-('2', '13', '3'),
-('3', '14', '1'),
-('4', '15', '4'),
-('5', '16', '2'),
-('6', '2', '3'),
-('7', '18', '3'),
-('8', '19', '3'),
-('9', '20', '1'),
-('11', '16', '1'),
-('12', '18', '1'),
-('13', '18', '2'),
-('14', '19', '2'),
-('15', '17', '4'),
-('16', '18', '4'),
-('17', '1', '1'),
-('18', '3', '1'),
-('19', '5', '1'),
-('20', '4', '2'),
-('21', '6', '2'),
-('22', '5', '3'),
-('23', '6', '3'),
-('24', '3', '4'),
-('25', '4', '4'),
-('26', '6', '4'),
-('27', '7', '1'),
-('28', '10', '1'),
-('29', '12', '1'),
-('30', '8', '2'),
-('31', '9', '2'),
-('32', '11', '2'),
-('33', '10', '3'),
-('34', '11', '3'),
-('35', '12', '3'),
-('36', '11', '4'),
-('37', '12', '4'),
-('38', '20', '2'),
-('39', '21', '2'),
-('40', '22', '2'),
-('41', '21', '3'),
-('42', '22', '3'),
-('43', '22', '4'),
-('44', '23', '1'),
-('45', '24', '2'),
-('46', '23', '4');
 
 -- --------------------------------------------------------
 
@@ -288,22 +289,6 @@ CREATE TABLE IF NOT EXISTS `intervenant` (
   PRIMARY KEY (`CODE`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Déclencheurs `intervenant`
---
-DROP TRIGGER IF EXISTS `before_delete_intervenant`;
-DELIMITER //
-CREATE TRIGGER `before_delete_intervenant` BEFORE DELETE ON `intervenant`
- FOR EACH ROW BEGIN
-	DELETE FROM acteur WHERE CODE = old.CODE;
-    DELETE FROM invite WHERE CODE = old.CODE;
-    DELETE FROM presentateur WHERE CODE = old.CODE;
-    DELETE FROM realisateur WHERE CODE = old.CODE;
-END
-//
-DELIMITER ;
-
--- --------------------------------------------------------
 --
 -- Contenu de la table `intervenant`
 --
@@ -341,6 +326,21 @@ INSERT INTO `intervenant` (`CODE`, `NOM`, `PRENOM`) VALUES
 ('8', 'Caine', 'Michael'),
 ('9', 'Cage', 'Nicolas');
 
+--
+-- Déclencheurs `intervenant`
+--
+DROP TRIGGER IF EXISTS `before_delete_intervenant`;
+DELIMITER //
+CREATE TRIGGER `before_delete_intervenant` BEFORE DELETE ON `intervenant`
+ FOR EACH ROW BEGIN
+	DELETE FROM acteur WHERE CODE = old.CODE;
+    DELETE FROM invite WHERE CODE = old.CODE;
+    DELETE FROM presentateur WHERE CODE = old.CODE;
+    DELETE FROM realisateur WHERE CODE = old.CODE;
+END
+//
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
@@ -355,6 +355,7 @@ CREATE TABLE IF NOT EXISTS `invite` (
   PRIMARY KEY (`CODE`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
 --
 -- Structure de la table `nationalite`
@@ -435,7 +436,7 @@ INSERT INTO `presentateur` (`CODE`, `PROFESSION`, `NOM`, `PRENOM`) VALUES
 ('28', 'Chirurgien-dentiste', NULL, NULL),
 ('29', 'Conseil en investissements finan', NULL, NULL);
 
-
+-- --------------------------------------------------------
 
 --
 -- Structure de la table `programme`
@@ -448,7 +449,7 @@ CREATE TABLE IF NOT EXISTS `programme` (
   `ID_ORIGINAIRE` char(32) NOT NULL,
   `NOM` char(32) DEFAULT NULL,
   `DUREE` time DEFAULT NULL,
-  `DESCRIPTION` char(32) DEFAULT NULL,
+  `DESCRIPTION` text,
   `ANNEE` int(11) DEFAULT NULL,
   `INEDIT` tinyint(4) DEFAULT NULL,
   `HD` tinyint(4) DEFAULT NULL,
@@ -463,52 +464,52 @@ CREATE TABLE IF NOT EXISTS `programme` (
 --
 
 INSERT INTO `programme` (`CODE`, `ID`, `ID_CORRESPOND`, `ID_ORIGINAIRE`, `NOM`, `DUREE`, `DESCRIPTION`, `ANNEE`, `INEDIT`, `HD`) VALUES
-('1', '2', '1', '4', 'Fast And Furious', '02:00:00', 'Action', 2015, 0, 1),
-('10', NULL, '1', '2', 'Taxi 3', '01:30:00', 'Action', 2003, 0, 0),
-('2', '2', '7', '7', 'Favelas', '02:00:00', 'Thriller', 2014, 0, 0),
-('3', NULL, '13', '11', 'Manchester city/Manchester Unite', '02:00:00', 'Sport', 2016, 0, 1),
-('4', NULL, '1', '2', 'Taxi 2', '01:30:00', 'Action', 2000, 1, 0),
-('5', '1', '1', '4', 'Divergente 2 : L''insurrection', '02:00:00', 'Action', 2015, 0, 1),
-('6', NULL, '2', '11', 'Enquete de foot', '01:00:00', 'Sport', 2016, 0, 1),
-('7', '2', '10', '12', 'Vikings', '00:40:00', 'Action', 2013, 1, 0),
-('8', '2', '10', '12', 'Vikings', '00:40:00', 'Action', 2013, 1, 0),
-('9', '2', '10', '12', 'Vikings', '00:40:00', 'Action', 2013, 0, 1),
-('11', '2', '1', '4', 'Fast And Furious', '02:00:00', 'Action', 2015, 0, 1),
-('12', NULL, '1', '2', 'Taxi 3', '01:30:00', 'Action', 2003, 0, 0),
+('1', '2', '1', '4', 'Fast And Furious', '02:00:00', 'Dominic Toretto est un ex-prisonnier désormais pilote de courses de rue. Il est entouré d''une bande composée de Letty, Vince, Jesse, Leon et de sa sœur Mia Toretto.', 2015, 0, 1),
+('10', NULL, '1', '2', 'Taxi 3', '01:30:00', 'Depuis huit mois, la police de Marseille est confrontée à un mystérieux gang en rollers déguisé en pères Noël. L''enquête piétine alors qu’Émilien et le commissaire Gibert multiplient les bourdes', 2003, 0, 0),
+('11', '2', '1', '4', 'Fast And Furious', '02:00:00', 'Dominic Toretto est un ex-prisonnier désormais pilote de courses de rue. Il est entouré d''une bande composée de Letty, Vince, Jesse, Leon et de sa sœur Mia Toretto.', 2015, 0, 1),
+('12', NULL, '1', '2', 'Taxi 3', '01:30:00', 'Depuis huit mois, la police de Marseille est confrontée à un mystérieux gang en rollers déguisé en pères Noël. L''enquête piétine alors qu’Émilien et le commissaire Gibert multiplient les bourdes', 2003, 0, 0),
 ('13', '2', '7', '7', 'Favelas', '02:00:00', 'Thriller', 2014, 0, 0),
-('14', NULL, '13', '11', 'Manchester city/Manchester Unite', '02:00:00', 'Sport', 2016, 0, 1),
-('15', NULL, '1', '2', 'Taxi 2', '01:30:00', 'Action', 2000, 1, 0),
-('16', '1', '1', '4', 'Divergente 2 : L''insurrection', '02:00:00', 'Action', 2015, 0, 1),
+('14', NULL, '13', '11', 'Manchester city/Manchester Unite', '02:00:00', 'Match de foot opposant les deux équipes', 2016, 0, 1),
+('15', NULL, '1', '2', 'Taxi 2', '01:30:00', 'Alors que Jean-Louis Schlesser et son copilote Henri Magne sont en course, Daniel arrive derrière avec son taxi et essaie de les doubler. En effet, il doit transporter au plus vite une femme enceinte prise de violentes contractions', 2000, 1, 0),
+('16', '1', '1', '4', 'Divergente 2 : L''insurrection', '02:00:00', 'Après la destruction du territoire de la faction des Altruistes par Jeanine, cheffe des Érudits ; Tris, Tobias, Caleb et Marcus se réfugient chez les Fraternels, attendant que la situation se calme', 2015, 0, 1),
 ('17', NULL, '2', '11', 'Enquete de foot', '01:00:00', 'Sport', 2016, 0, 1),
-('18', '2', '10', '12', 'Vikings', '00:40:00', 'Action', 2013, 1, 0),
-('19', '2', '10', '12', 'Vikings', '00:40:00', 'Action', 2013, 1, 0),
-('20', '2', '1', '4', 'Fast And Furious', '02:00:00', 'Action', 2015, 0, 1),
-('21', NULL, '1', '2', 'Taxi 3', '01:30:00', 'Action', 2003, 0, 0),
+('18', '2', '10', '12', 'Vikings', '00:40:00', 'La série suit les exploits d''un groupe de Vikings mené par Ragnar Lothbrok, l''un des plus populaires héros vikings de tous les temps au destin semi-légendaire. Ragnar serait d''origine danoise, suédoise ou encore norvégienne selon les sources', 2013, 1, 0),
+('19', '2', '10', '12', 'Vikings', '00:40:00', 'La série suit les exploits d''un groupe de Vikings mené par Ragnar Lothbrok, l''un des plus populaires héros vikings de tous les temps au destin semi-légendaire. Ragnar serait d''origine danoise, suédoise ou encore norvégienne selon les sources', 2013, 1, 0),
+('2', '2', '7', '7', 'Favelas', '02:00:00', 'Thriller', 2014, 0, 0),
+('20', '2', '1', '4', 'Fast And Furious', '02:00:00', 'Dominic Toretto est un ex-prisonnier désormais pilote de courses de rue. Il est entouré d''une bande composée de Letty, Vince, Jesse, Leon et de sa sœur Mia Toretto.', 2015, 0, 1),
+('21', NULL, '1', '2', 'Taxi 3', '01:30:00', 'Depuis huit mois, la police de Marseille est confrontée à un mystérieux gang en rollers déguisé en pères Noël. L''enquête piétine alors qu’Émilien et le commissaire Gibert multiplient les bourdes', 2003, 0, 0),
 ('22', '2', '7', '7', 'Favelas', '02:00:00', 'Thriller', 2014, 0, 0),
 ('23', NULL, '13', '11', 'Manchester city/Manchester Unite', '02:00:00', 'Sport', 2016, 0, 1),
-('24', NULL, '1', '2', 'Taxi 2', '01:30:00', 'Action', 2000, 1, 0),
-('25', '1', '1', '4', 'Divergente 2 : L''insurrection', '02:00:00', 'Action', 2015, 0, 1),
+('24', NULL, '1', '2', 'Taxi 2', '01:30:00', 'Alors que Jean-Louis Schlesser et son copilote Henri Magne sont en course, Daniel arrive derrière avec son taxi et essaie de les doubler. En effet, il doit transporter au plus vite une femme enceinte prise de violentes contractions', 2000, 1, 0),
+('25', '1', '1', '4', 'Divergente 2 : L''insurrection', '02:00:00', 'Après la destruction du territoire de la faction des Altruistes par Jeanine, cheffe des Érudits ; Tris, Tobias, Caleb et Marcus se réfugient chez les Fraternels, attendant que la situation se calme', 2015, 0, 1),
 ('26', NULL, '2', '11', 'Enquete de foot', '01:00:00', 'Sport', 2016, 0, 1),
-('27', '2', '10', '12', 'Vikings', '00:40:00', 'Action', 2013, 1, 0),
-('28', '2', '10', '12', 'Vikings', '00:40:00', 'Action', 2013, 1, 0),
-('29', '2', '1', '4', 'Fast And Furious', '02:00:00', 'Action', 2015, 0, 1),
-('30', NULL, '1', '2', 'Taxi 3', '01:30:00', 'Action', 2003, 0, 0),
+('27', '2', '10', '12', 'Vikings', '00:40:00', 'La série suit les exploits d''un groupe de Vikings mené par Ragnar Lothbrok, l''un des plus populaires héros vikings de tous les temps au destin semi-légendaire. Ragnar serait d''origine danoise, suédoise ou encore norvégienne selon les sources', 2013, 1, 0),
+('28', '2', '10', '12', 'Vikings', '00:40:00', 'La série suit les exploits d''un groupe de Vikings mené par Ragnar Lothbrok, l''un des plus populaires héros vikings de tous les temps au destin semi-légendaire. Ragnar serait d''origine danoise, suédoise ou encore norvégienne selon les sources', 2013, 1, 0),
+('29', '2', '1', '4', 'Fast And Furious', '02:00:00', 'Dominic Toretto est un ex-prisonnier désormais pilote de courses de rue. Il est entouré d''une bande composée de Letty, Vince, Jesse, Leon et de sa sœur Mia Toretto.', 2015, 0, 1),
+('3', NULL, '13', '11', 'Manchester city/Manchester Unite', '02:00:00', 'Sport', 2016, 0, 1),
+('30', NULL, '1', '2', 'Taxi 3', '01:30:00', 'Depuis huit mois, la police de Marseille est confrontée à un mystérieux gang en rollers déguisé en pères Noël. L''enquête piétine alors qu’Émilien et le commissaire Gibert multiplient les bourdes', 2003, 0, 0),
 ('31', '2', '7', '7', 'Favelas', '02:00:00', 'Thriller', 2014, 0, 0),
 ('32', NULL, '13', '11', 'Manchester city/Manchester Unite', '02:00:00', 'Sport', 2016, 0, 1),
-('33', NULL, '1', '2', 'Taxi 2', '01:30:00', 'Action', 2000, 1, 0),
-('34', '1', '1', '4', 'Divergente 2 : L''insurrection', '02:00:00', 'Action', 2015, 0, 1),
+('33', NULL, '1', '2', 'Taxi 2', '01:30:00', 'Alors que Jean-Louis Schlesser et son copilote Henri Magne sont en course, Daniel arrive derrière avec son taxi et essaie de les doubler. En effet, il doit transporter au plus vite une femme enceinte prise de violentes contractions', 2000, 1, 0),
+('34', '1', '1', '4', 'Divergente 2 : L''insurrection', '02:00:00', 'Après la destruction du territoire de la faction des Altruistes par Jeanine, cheffe des Érudits ; Tris, Tobias, Caleb et Marcus se réfugient chez les Fraternels, attendant que la situation se calme', 2015, 0, 1),
 ('35', NULL, '2', '11', 'Enquete de foot', '01:00:00', 'Sport', 2016, 0, 1),
-('36', '2', '10', '12', 'Vikings', '00:40:00', 'Action', 2013, 1, 0),
-('37', '2', '10', '12', 'Vikings', '00:40:00', 'Action', 2013, 1, 0),
-('38', '2', '1', '4', 'Fast And Furious', '02:00:00', 'Action', 2015, 0, 1),
-('39', NULL, '1', '2', 'Taxi 3', '01:30:00', 'Action', 2003, 0, 0),
+('36', '2', '10', '12', 'Vikings', '00:40:00', 'La série suit les exploits d''un groupe de Vikings mené par Ragnar Lothbrok, l''un des plus populaires héros vikings de tous les temps au destin semi-légendaire. Ragnar serait d''origine danoise, suédoise ou encore norvégienne selon les sources', 2013, 1, 0),
+('37', '2', '10', '12', 'Vikings', '00:40:00', 'La série suit les exploits d''un groupe de Vikings mené par Ragnar Lothbrok, l''un des plus populaires héros vikings de tous les temps au destin semi-légendaire. Ragnar serait d''origine danoise, suédoise ou encore norvégienne selon les sources', 2013, 1, 0),
+('38', '2', '1', '4', 'Fast And Furious', '02:00:00', 'Dominic Toretto est un ex-prisonnier désormais pilote de courses de rue. Il est entouré d''une bande composée de Letty, Vince, Jesse, Leon et de sa sœur Mia Toretto.', 2015, 0, 1),
+('39', NULL, '1', '2', 'Taxi 3', '01:30:00', 'Depuis huit mois, la police de Marseille est confrontée à un mystérieux gang en rollers déguisé en pères Noël. L''enquête piétine alors qu’Émilien et le commissaire Gibert multiplient les bourdes', 2003, 0, 0),
+('4', NULL, '1', '2', 'Taxi 2', '01:30:00', 'Alors que Jean-Louis Schlesser et son copilote Henri Magne sont en course, Daniel arrive derrière avec son taxi et essaie de les doubler. En effet, il doit transporter au plus vite une femme enceinte prise de violentes contractions', 2000, 1, 0),
 ('40', '2', '7', '7', 'Favelas', '02:00:00', 'Thriller', 2014, 0, 0),
 ('41', NULL, '13', '11', 'Manchester city/Manchester Unite', '02:00:00', 'Sport', 2016, 0, 1),
-('42', NULL, '1', '2', 'Taxi 2', '01:30:00', 'Action', 2000, 1, 0),
-('43', '1', '1', '4', 'Divergente 2 : L''insurrection', '02:00:00', 'Action', 2015, 0, 1),
+('42', NULL, '1', '2', 'Taxi 2', '01:30:00', 'Alors que Jean-Louis Schlesser et son copilote Henri Magne sont en course, Daniel arrive derrière avec son taxi et essaie de les doubler. En effet, il doit transporter au plus vite une femme enceinte prise de violentes contractions', 2000, 1, 0),
+('43', '1', '1', '4', 'Divergente 2 : L''insurrection', '02:00:00', 'Après la destruction du territoire de la faction des Altruistes par Jeanine, cheffe des Érudits ; Tris, Tobias, Caleb et Marcus se réfugient chez les Fraternels, attendant que la situation se calme', 2015, 0, 1),
 ('44', NULL, '2', '11', 'Enquete de foot', '01:00:00', 'Sport', 2016, 0, 1),
-('45', '2', '10', '12', 'Vikings', '00:40:00', 'Action', 2013, 1, 0),
-('46', '2', '10', '12', 'Vikings', '00:40:00', 'Action', 2013, 1, 0);
+('45', '2', '10', '12', 'Vikings', '00:40:00', 'La série suit les exploits d''un groupe de Vikings mené par Ragnar Lothbrok, l''un des plus populaires héros vikings de tous les temps au destin semi-légendaire. Ragnar serait d''origine danoise, suédoise ou encore norvégienne selon les sources', 2013, 1, 0),
+('46', '2', '10', '12', 'Vikings', '00:40:00', 'La série suit les exploits d''un groupe de Vikings mené par Ragnar Lothbrok, l''un des plus populaires héros vikings de tous les temps au destin semi-légendaire. Ragnar serait d''origine danoise, suédoise ou encore norvégienne selon les sources', 2013, 1, 0),
+('5', '1', '1', '4', 'Divergente 2 : L''insurrection', '02:00:00', 'Après la destruction du territoire de la faction des Altruistes par Jeanine, cheffe des Érudits ; Tris, Tobias, Caleb et Marcus se réfugient chez les Fraternels, attendant que la situation se calme', 2015, 0, 1),
+('6', NULL, '2', '11', 'Enquete de foot', '01:00:00', 'Sport', 2016, 0, 1),
+('7', '2', '10', '12', 'Vikings', '00:40:00', 'La série suit les exploits d''un groupe de Vikings mené par Ragnar Lothbrok, l''un des plus populaires héros vikings de tous les temps au destin semi-légendaire. Ragnar serait d''origine danoise, suédoise ou encore norvégienne selon les sources', 2013, 1, 0),
+('8', '2', '10', '12', 'Vikings', '00:40:00', 'La série suit les exploits d''un groupe de Vikings mené par Ragnar Lothbrok, l''un des plus populaires héros vikings de tous les temps au destin semi-légendaire. Ragnar serait d''origine danoise, suédoise ou encore norvégienne selon les sources', 2013, 1, 0),
+('9', '2', '10', '12', 'Vikings', '00:40:00', 'La série suit les exploits d''un groupe de Vikings mené par Ragnar Lothbrok, l''un des plus populaires héros vikings de tous les temps au destin semi-légendaire. Ragnar serait d''origine danoise, suédoise ou encore norvégienne selon les sources', 2013, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -540,7 +541,7 @@ INSERT INTO `realisateur` (`CODE`, `NBFILM`, `NOM`, `PRENOM`) VALUES
 ('30', 45, NULL, NULL),
 ('31', 26, NULL, NULL);
 
-
+-- --------------------------------------------------------
 
 --
 -- Structure de la table `signaletique`
